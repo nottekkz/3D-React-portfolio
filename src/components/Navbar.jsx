3b9e71391e8f6cb1c20d0cd6102181e65479cbce
Muhammed Hussain <mh7247800@gmail.com>
2023-11-01 02:45:01 +0500

    Update
    Updated home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,10 @@ const Navbar = () => {
         }}
           >
             <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-            <p className="text-white text-[18px] font-bold cursor-pointer">Hussain <span className="sm:block hidden">| Javascript Mastery</span> </p>
+            <p className="text-white text-[18px] font-bold cursor-pointer flex">
+                Hussain &nbsp;
+                <span className="sm:block hidden">| JS Mastery</span> 
+              </p>
           </Link>
           <ul className="list-none hidden sm:flex flex-row gap-10">
             {navLinks.map((Link) => (
