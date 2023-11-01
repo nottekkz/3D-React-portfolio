eeb2e6de08438dd9d1f4aab8b97a94cf4400f868
Muhammed Hussain <mh7247800@gmail.com>
2023-11-02 03:04:47 +0500

    Update
    Updated the 3D model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,12 +2,45 @@ import { Suspense, useEffect, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
-import CanvasLoader from '../Loader'
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+      scale={0.75}
+      position={[0, -3.25, -1.5]}
+      rotation={[-0.01, -0.2, -0.1]}
+      />
+      </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <canvas
+    frameloop="demand"
+    shadows
+    camera={{ position: [20, 3, 5], fov: 25 }}
+    gL={{ preserveDrawingBuffer: true }}
+    >
+    <Suspense fallback={<CanvasLoader />}>
+      <OrbitControls 
+      enableZoom={false}
+      maxPoLarAngle={Math.PI / 2}
+      minPolarAngle={Math.PI / 2}
+      />
+      <Computers />
+    </Suspense>
+
+    <Preload all />
+    </canvas>
+  )
+}
+
+export default ComputersCanvas;
